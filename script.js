60706c7de49df4e30fe6425864fe2bf14d3aa73e
Sergio Villanueva <sivillanueva@outlook.com>
2021-11-03 13:47:12 -0600

    se agregó mapa y contacto por whats

---
+++ script.js
@@ -0,0 +1,34 @@
+function isMobile() {
+    if (sessionStorage.desktop)
+        return false;
+    else if (localStorage.mobile)
+        return true;
+    var mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
+    for (var i in mobile)
+        if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
+    return false;
+}
+
+const formulario = document.querySelector('#formulario');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const telefono = '524493346095';
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
+    buttonSubmit.disabled = true
+    setTimeout(() => {
+        let nombre = document.querySelector('#nombre').value
+        let contenido = document.querySelector('#mensaje-wa').value
+        let mensaje = 'send?phone=' + telefono + '&text=Hola Teresina.%0AMi nombre es:%0A' + nombre +'%0AMi mensaje es:%0A'+ contenido + ''
+        if (isMobile()) {
+            window.open(urlMobile + mensaje, '_blank')
+        } else {
+            window.open(urlDesktop + mensaje, '_blank')
+        }
+        buttonSubmit.innerHTML = '<i class="fab fa-whatsapp"></i> Enviar WhatsApp'
+        buttonSubmit.disabled = false
+    }, 300);
+});
